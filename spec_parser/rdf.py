b18103cf72fb4a6cae0a0a259428d27ac86f9238
Gary O'Neall <gary@sourceauditor.com>
2024-12-09 16:30:44 -0800

    Change creaeted by URIs in individuals to SpdxOrganization
    Fixes #166

--- spec_parser/rdf.py
+++ spec_parser/rdf.py
@@ -219,7 +219,7 @@ def gen_rdf_individuals(model, g):
         g.add((ci_node, RDF.type, ci_ref("CreationInfo")))
         g.add((ci_node, RDFS.comment, Literal("This individual element was defined by the spec.", lang="en")))
         g.add((ci_node, ci_ref("created"), Literal("2024-11-22T03:00:01Z", datatype=XSD.dateTimeStamp)))
-        g.add((ci_node, ci_ref("createdBy"), URIRef("https://spdx.org/")))
+        g.add((ci_node, ci_ref("createdBy"), ci_ref("SpdxOrganization")))
         g.add((ci_node, ci_ref("specVersion"), Literal("3.0.1")))
         node = URIRef(i.iri)
         g.add((node, RDF.type, OWL.NamedIndividual))
